dde46395083628158c94d143c32493b03f046c84
Ap-chaz <alvanpeter29@gmail.com>
2026-08-13 18:24:09 +0300

    update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -42,12 +42,12 @@ products.forEach((product) => {
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
+              ${(product.rating.count)}
             </div>
           </div>
 
           <div class="product-price">
-            $${product.priceCents / 100}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
