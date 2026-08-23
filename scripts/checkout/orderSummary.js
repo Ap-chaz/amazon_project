38bfc83801843d963c564c5bfe12a22fc9487280
Ap-chaz <alvanpeter29@gmail.com>
2026-08-23 16:56:36 +0300

    update

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -35,7 +35,9 @@ cart.forEach((cartItem) => {
     );
   
    cartSummaryHTML += `
-  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+  <div class="cart-item-container 
+  js-cart-item-container
+  js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -51,7 +53,8 @@ cart.forEach((cartItem) => {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity
+                js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
