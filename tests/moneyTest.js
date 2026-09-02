58df1d132ff8941d0f984f432e6ef4194adbe723
Ap-chaz <alvanpeter29@gmail.com>
2026-09-02 17:29:12 +0300

    Create automated tests for formstCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,4 +1,25 @@
 import {formatCurrency} from "../scripts/utils/money.js";
 
-
+if (formatCurrency(2095) === "20.95") {
+    console.log("formatCurrency test passed");
+} else {
+    console.log("formatCurrency test failed");
+}
  
+if (formatCurrency(0) === "0.00") {
+    console.log("formatCurrency test passed");
+} else {
+    console.log("formatCurrency test failed");
+}
+
+if (formatCurrency(2000.5) === "20.01") {
+    console.log("formatCurrency test passed");
+} else {
+    console.log("formatCurrency test failed");
+}
+
+if (formatCurrency(2000.4) === "20.00") {
+    console.log("formatCurrency test passed");
+} else {
+    console.log("formatCurrency test failed");
+}
